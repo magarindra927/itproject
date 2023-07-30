de7b77a80b0bb099c5dd793bf3dcf7e7c3d39d4c
indra <magarindra927@gmail.com>
2023-07-30 07:46:47 +0545

    update

--- data/settings.py
+++ data/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_ROOT=BASE_DIR / 'media',
+MEDIA_ROOT=BASE_DIR / 'media'
 MEDIA_URL='/media/'
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',    
+    BASE_DIR / 'static'   
 ]
 STATIC_URL = 'static/'
 
